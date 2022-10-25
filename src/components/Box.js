dde074af30fbe507a2e820d232414ed1d84ec03a
mimi7427 <umudede22@gmail.com>
2022-10-25 09:52:54 +0200

    my assignment

---
+++ src/components/Box.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Box() {
+
+  return(
+    <>
+    <div className="box-wrapper">
+         <div className="box-item"> 
+           <div className="p">
+               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque tempora quam provident vitae fuga. Quasi, amet doloribus? Omnis optio, voluptatem repudiandae nostrum unde voluptas ad. Tempora nesciunt ipsa iusto distinctio!
+           </div>
+         </div>
+       </div>
+    </>
+    );
+}
+export default Box
